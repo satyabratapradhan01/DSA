aa91cf776752c04c19ccf9e84f995fe75b28dfa1
satyabratapradhan <satyabratapradhann@gmail.com>
2025-07-24 23:46:54 +0530

    condition

---
+++ conditional_statements/LeapYear.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year");
+        int year = sc.nextInt();
+        if(year % 4 == 0) {
+            System.out.println("Not a Leap Year");
+        }else if(year % 100 == 0){
+            System.out.println("Not a Leap Year");
+        }else if(year % 400 == 0){
+            System.out.println("Not a Leap Year");
+        }else{
+            System.out.println("Leap Year");
+        }
+    }
+}
---
+++ conditional_statements/Ternart.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ternart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A Number");
+        int number = sc.nextInt();
+
+        String Type = ((number%2) == 0) ? "Even" : "Odd";
+        System.out.println(Type);
+    }
+}
