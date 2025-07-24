4ad3b8835df723371d6ccb66176c8cbda3600035
satyabratapradhan <satyabratapradhann@gmail.com>
2025-07-24 09:45:31 +0530

    conditional

---
+++ conditional_statements/LargestNumOfTwo.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class LargestNumOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int firstNum = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int secondNum = sc.nextInt();
+
+        if(firstNum > secondNum){
+            System.out.println(firstNum + " is grete");
+        }else{
+            System.out.println(secondNum + " is greter");
+        }
+    }
+}
